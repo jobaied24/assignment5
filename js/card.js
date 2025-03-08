49f7e333c4181c393d1dc46b04d2037f419a0fa5
hero00 <jobaied543@gmail.com>
2025-03-09 01:02:35 +0600

    completed clear History section

--- js/card.js
+++ js/card.js
@@ -5,6 +5,8 @@ let convertedTask = parseInt(task);
 const checkbox = document.getElementById('checkbox').innerText;
 let convertedCheckbox = parseInt(checkbox);
 
+const history=document.getElementById('historySection');
+
 
 function clickById(id){
 document.getElementById(id).addEventListener('click',function(event){
@@ -20,9 +22,8 @@ document.getElementById(id).addEventListener('click',function(event){
    
 
 //    history section
-   const history=document.getElementById('historySection');
    const div=document.createElement('div');
-   div.classList.add("bg-blue-100","m-3","p-2","rounded-md")
+   div.classList.add("bg-blue-100","m-3","p-2","rounded-md","history-entires")
 
    const card=this.closest("div.bg-blue-100");
    const cardTitle=card.querySelector('h2').innerText;
@@ -35,7 +36,6 @@ document.getElementById(id).addEventListener('click',function(event){
 })
 }
 
-
 // clickById('card1')
 // clickById('card2')
 // clickById('card3')
@@ -44,3 +44,14 @@ document.getElementById(id).addEventListener('click',function(event){
 // clickById('card6')
 
 ['card1','card2','card3','card4','card5','card6'].forEach(clickById);
+
+
+
+// clear history
+document.getElementById('history-btn').addEventListener('click',function(event){
+    event.preventDefault();
+    const dltHistory=document.querySelectorAll('.history-entires');
+    dltHistory.forEach(function(div){
+        div.remove();
+    })
+})
